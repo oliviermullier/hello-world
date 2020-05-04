6212bac4e2fd215774787b1f0dc1fac21c05d822
Olivier Mullier <olivier.mullier@ensta.fr>
2020-05-04 10:38:23 +0200

    ajout d'un exemple d'utilisation de SDL_Image

--- minimal-sdl/README.md
+++ minimal-sdl/README.md
@@ -1,14 +1,14 @@
 # Exemple d'utilisation de la SDL
  
-Ceci est un exemple minimal d'utilisation de la SDL.
-Il ne fait qu'afficher une fenêtre et permet de voir si la SDL a bien été installée.
+Ceci est un exemple minimal d'utilisation de la SDL ainsi que SDL_Image.
+Il ne fait qu'afficher une fenêtre ainsi qu'une image png et permet de voir si la SDL a bien été installée.
 
 ## Compilation
 Dans le fichier src, la compilation se fait à l'aide de la commande
 
 ```console
-olivier@mullier:~/Directory/to/src$ gcc main.c $(sdl-config --cflags --libs) -o minimal.x
+olivier@mullier:~/Directory/to/src$ gcc main.c $(sdl-config --cflags --libs) -lSDL_image -o minimal.x
 ```
 
 L'éxecutable minimal.x est alors créé.
-L'exécution de minimal.x entraîne l'affichage d'une fenêtre noire que l'on peut fermer avec la croix.
+L'exécution de minimal.x entraîne l'affichage d'une fenêtre noire contenant 2 fois l'image logo.png que l'on peut fermer avec la croix.
--- minimal-sdl/src/main.c
+++ minimal-sdl/src/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL.h>
+#include <SDL_image.h>
+
 
 void pause()
 {
@@ -22,7 +24,27 @@ int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
  
-    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    SDL_Surface * ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    
+    // chargement de l'image logo.png
+    SDL_Surface *image = IMG_Load("logo.png");
+    if(!image) {
+        printf("IMG_Load: %s\n", IMG_GetError());//gestion des erreurs
+        SDL_Quit(); // Arrêt de la SDL
+        return -1;
+}
+    
+    SDL_Rect position_image1, position_image2;
+    
+    position_image1.x = 20;
+    position_image1.y = 20;
+    
+    SDL_BlitSurface(image, NULL, ecran, &position_image1);
+    
+    position_image2.x = 240;
+    position_image2.y = 20;
+    
+    SDL_BlitSurface(image, NULL, ecran, &position_image2);
     
     pause(); // Mise en pause du programme
  
